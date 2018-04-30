a786bd669c48fad64791d6c5904a751496609d94
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2018-04-29 21:26:57 -0500

    Presentación de barra lateral para los doctores.

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>DOCTOR - Responsive HTML &amp; Bootstrap Template</title>
+        <title>DOCTOR</title>
         <link rel="stylesheet" href="css/font-awesome.css">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/jquery-ui.css">
         <link rel="stylesheet" href="codebase/dhtmlxscheduler.css">
         <link rel="stylesheet" href="css/dhtmlscheduler_eos.css">
+        <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css" />
+        <link rel="stylesheet" href="css/reserva.css" />
+
+
+
         <!--[if lt IE 9]>
         <script src="js/html5shiv.js"></script>
         <script src="js/respond.min.js"></script>
@@ -21,24 +26,43 @@
         <!-- header section -->
         <div class="container-fluid">
             <?php include "header.php"; ?> 
-            <div class="dhx_cal_container panel" id="scheduler_here">
-                <div class="dhx_cal_navline">
-                    <div class="dhx_cal_prev_button">&nbsp;</div>
-                    <div class="dhx_cal_next_button">&nbsp;</div>
-                    <div class="dhx_cal_today_button"></div>
-                    <div class="dhx_cal_date"></div>
-                    <div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
-                    <div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
-                    <div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
-                </div>
-                <div class="dhx_cal_header">
-                </div>
-                <div class="dhx_cal_data">
+            <div class="wrapper">
+                <nav id="sidebar">
+                    <!-- Sidebar Header -->
+                    <div class="sidebar-header">
+                        <h3>Reserva de Horas</h3>
+                    </div>
+
+                    <!-- Sidebar Links -->
+                    <ul class="list-unstyled components">
+                        <li class="active"><a href="#">Inicio</a></li>
+                        <li><a href="#">Acerca de</a></li>
+                        <li><!-- Link with dropdown items -->
+                            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Doctores</a>
+                            <ul class="collapse list-unstyled" id="homeSubmenu">
+                            </ul>
+                    </ul>
+                </nav>
+                <div id="content">
+                    <div class="dhx_cal_container panel" id="scheduler_here">
+                        <div class="dhx_cal_navline">
+                            <div class="dhx_cal_prev_button">&nbsp;</div>
+                            <div class="dhx_cal_next_button">&nbsp;</div>
+                            <div class="dhx_cal_today_button"></div>
+                            <div class="dhx_cal_date"></div>
+                            <div class="dhx_cal_tab" name="day_tab" style="right:204px;"></div>
+                            <div class="dhx_cal_tab" name="week_tab" style="right:140px;"></div>
+                            <div class="dhx_cal_tab" name="month_tab" style="right:76px;"></div>
+                        </div>
+                        <div class="dhx_cal_header">
+                        </div>
+                        <div class="dhx_cal_data">
+                        </div>
+                    </div>
                 </div>
             </div>
-
             <!-- footer starts here -->
-            <?php include "footer.php"; ?>
+            <!--<?php include "footer.php"; ?>-->
         </div>
         <!-- script tags
         ============================================================= -->
@@ -48,5 +72,7 @@
         <script src="js/dbAccess.js"></script>
         <script src="codebase/dhtmlxscheduler.js"></script>
         <script src="codebase/locale/locale_es.js"></script>
+        <script src="js/jquery.mCustomScrollbar.js"></script>
+        <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
     </body>
 </html>
--- js/dbAccess.js
+++ js/dbAccess.js
@@ -1,12 +1,45 @@
-function init() {
-    scheduler.config.multi_day = true;
-    scheduler.config.xml_date = "%Y-%m-%d %H:%i";
-    scheduler.init('scheduler_here', new Date(2018, 0, 1), "week");
-    //scheduler.load("./data/events.xml");
-
-}
-
 $().ready(function () {
-    init();
+    scheduler.init('scheduler_here', new Date(),"month");
+    $.ajax({
+        type: "POST",
+        url: "src/doctors.php",
+        data: "{}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+
+            var line = "";
+            $.each(msg, function (index, item) {
+                
+                line = line + "<li><a href='#'>" + item.personalNombre+"</a></li>";
+                
+            });
+            $("#homeSubmenu").append(line);
+            //$("#cmbdoctor").get(0).options[$("#cmbdoctor").get(0).options.length] = new Option(item.personalNombre, item.personalId);
+
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+            alert("-- " + xhr.responseText + " --");
+        }
+    });
 });
 
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});

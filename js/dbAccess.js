9de36f4956fefb13f99d15e10184659f78746f78
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2018-05-01 20:18:49 -0500

    Avance en la revisión de reserva de horas por parte de las secretarias.

--- js/dbAccess.js
+++ js/dbAccess.js
@@ -1,5 +1,21 @@
 $().ready(function () {
-    scheduler.init('scheduler_here', new Date(),"month");
+    var step = 20;
+    var format = scheduler.date.date_to_str("%H:%i");
+
+    scheduler.config.hour_size_px = (60 / step) * 22;
+    scheduler.templates.hour_scale = function (date) {
+        html = "";
+        for (var i = 0; i < 60 / step; i++) {
+            html += "<div style='height:22px;line-height:22px;'>" + format(date) + "</div>";
+            date = scheduler.date.add(date, step, "minute");
+        }
+        return html;
+    }
+    scheduler.templates.lightbox_header = function(start, end, event){
+			return "Detalle para "+event.text;
+		}
+    
+    scheduler.init('scheduler_here', new Date(), "month");
     $.ajax({
         type: "POST",
         url: "src/doctors.php",
@@ -10,13 +26,9 @@ $().ready(function () {
 
             var line = "";
             $.each(msg, function (index, item) {
-                
-                line = line + "<li><a href='#'>" + item.personalNombre+"</a></li>";
-                
+                line = line + "<li><a href='javascript:selectedDoctor(" + item.personalId + ")'>" + item.personalNombre + "</a></li>";
             });
             $("#homeSubmenu").append(line);
-            //$("#cmbdoctor").get(0).options[$("#cmbdoctor").get(0).options.length] = new Option(item.personalNombre, item.personalId);
-
         },
         error: function (xhr, ajaxOptions, thrownError) {
             alert(xhr.status);
@@ -26,10 +38,42 @@ $().ready(function () {
     });
 });
 
+
+function selectedDoctor(idDoctor) {
+
+    alert(idDoctor);
+
+    $.ajax({
+        type: "GET",
+        url: "src/reserva/obtener_horas_reservadas_x_doctor.php",
+        data: {"id_doctor": idDoctor},
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            scheduler.clearAll();
+            var events = [];
+            var id = 1;
+            $.each(msg, function (index, item) {
+                var item = {"id": id, "text": item.pacienteNombre, "start_date": item.fecha + " " + item.horainicio, "end_date": item.fecha + " " + item.horatermino, "rut": item.pacienteRut + "-" + item.pacienteDv, "email": item.pacienteEmail, "telefono": item.pacienteFono};
+                events.push(item);
+                id++;
+            });
+            scheduler.parse(events, "json");//takes the name and format of the data source
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+            alert("-- " + xhr.responseText + " --");
+        }
+    });
+
+}
+
 $(document).ready(function () {
 
     $("#sidebar").mCustomScrollbar({
-         theme: "minimal"
+        //theme: "minimal"
+        theme: "dark"
     });
 
     $('#sidebarCollapse').on('click', function () {
---
+++ src/reserva/obtener_horas_reservadas_x_doctor.php
@@ -0,0 +1,54 @@
+<?php
+// Obtiene horarios de los doctores que son de una especialidad específica  en una sede en partícular
+include_once(dirname(__FILE__).'/../global.php');
+
+$fecha = date("Y-m-d");
+//$fecha =  date("Y-m-d", strtotime("2017-12-07"));
+if( isset($_GET["fecha"]) )
+{
+    $fecha = utf8_encode($_GET["fecha"]);
+}
+$hora = date("H:i:s");
+//$hora = date("H:i:s", strtotime("08:00:00"));
+if( isset($_GET["hora"]) )
+{
+    $hora = utf8_encode($_GET["hora"]);
+}
+
+$id_doctor = 0;
+//$hora = date("H:i:s", strtotime("08:00:00"));
+if( isset($_GET["id_doctor"]) )
+{
+    $id_doctor = utf8_encode($_GET["id_doctor"]);
+}
+ 
+
+$sql =  "SELECT reserva.personalId, personalNombre, date_format(fecha, '%m/%d/%Y') as fecha, time_format(horainicio, '%H:%i') as horainicio, time_format(DATE_ADD(horainicio, INTERVAL 20 MINUTE),'%H:%i') as horatermino, pacienteRut, pacienteDv, pacienteNombre, pacienteEmail, pacienteFono "; 
+$sql = $sql. "FROM reserva, personal ";
+$sql = $sql. "WHERE 	personal.personalId = reserva.personalId and ";
+$sql = $sql. "		EXTRACT(MONTH FROM fecha) = EXTRACT(MONTH FROM '$fecha') and ";
+$sql = $sql. "        EXTRACT(YEAR FROM fecha) = EXTRACT(YEAR FROM '$fecha')  and ";
+$sql = $sql. "        personal.personalId = $id_doctor";
+        
+if( isset($_GET["sede"]) )
+{
+    $sede = utf8_encode($_GET["sede"]);
+	$sql = $sql. " and sedeId=".$sede;
+}
+
+$conexion = mysqli_connect(DB_HOST, DB_USER, DB_PWD, DB_NAME);
+mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
+
+if(!$result = mysqli_query($conexion, $sql)) die();
+
+$rawdata = array(); //creamos un array
+
+$i=0;
+while($row = mysqli_fetch_array($result))
+{
+	$rawdata[$i] = $row;
+	$i++;
+}
+$close = mysqli_close($conexion);
+echo json_encode($rawdata);
+?>
